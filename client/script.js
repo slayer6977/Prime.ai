7193e56c09e2d62e56b476aa5b6486825dc1b4a4
Prime.ai <https://github.com/slayer6977/Prime.ai.git>
2023-03-16 20:41:03 -0500

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://https-github-com-slayer6977-prime-ai.onrender.com', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
